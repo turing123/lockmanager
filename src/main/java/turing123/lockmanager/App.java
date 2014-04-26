f9cf03f198d2b88178a381f6ce9c781c4b1fee8a
Xiaoyan Chen <cxy123@gmail.com>
2014-04-26 23:28:19 +0800

    init project files

---
+++ src/main/java/turing123/lockmanager/App.java
@@ -0,0 +1,13 @@
+package turing123.lockmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
